51e7ad6ab2c95ef58aad20f2ae4ff8c11bcce05a
robbysuganda <robbyxsuganda@gmail.com>
2025-05-27 23:31:17 +0700

    feat: global error handler

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,43 @@
 import express from "express";
-import router from "./routes/api";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+import router from "./routes/api";
+
 import db from "./utils/database";
 import docs from "./docs/route";
+import errorMiddleware from "./middlewares/error.middleware";
 
 async function init() {
   try {
+    const PORT = 3000;
     const result = await db();
-    console.log("Database status: ", result);
+
+    console.log("database status: ", result);
 
     const app = express();
-    const PORT = 3000;
 
     app.use(cors());
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.get("/", (req, res) => {
-      // res.send("Hello World");
-      res.status(200).json({ message: "Server in running!", data: null });
-    });
-
-    app.get("/dummy", (req, res) => {
-      // res.send("Hello World");
-      res.status(200).json({ message: "Hello World!!!", data: null });
+      res.status(200).json({
+        message: "Server is running",
+        data: null,
+      });
     });
 
     app.use("/api", router);
-
     docs(app);
+
+    app.use(errorMiddleware.serverRoute());
+    app.use(errorMiddleware.serverError());
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
 
